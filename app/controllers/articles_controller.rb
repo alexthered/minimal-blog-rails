9dd82d0a768eb10b53892c06384dbe53dc7ea6bd
Hiep Doan <hiep.dv179@gmail.com>
2017-05-04 19:54:00 +0200

    basic CRUD for articles model
    - create, show, index page and controller handler for article model

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,27 @@
+class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    @article = Article.new(article_param)
+  
+    @article.save
+    redirect_to @article
+  end
+  
+  
+  protected
+    def article_param
+      params.require(:article).permit(:title, :text)
+    end
+end
